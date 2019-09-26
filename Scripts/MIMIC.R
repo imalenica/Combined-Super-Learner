7319f1ec09fa92c233a738bb8c3be74234b0d69c
rachaelvphillips <rachaelvphillips@gmail.com>
2019-09-26 11:06:36 -0700

    fixes to avoid later errors

--- Scripts/MIMIC.R
+++ Scripts/MIMIC.R
@@ -9,39 +9,29 @@ source(here::here("R", "utils_mimic.R"))
 load(here::here("Data","mimic_all.Rdata"))
 
 ################################################################################
-# only include subjects with 8 hours of data at most 3 min time gap between
+# only include subjects with 5 hours of data at most 1 min time gap between
 # two consecutive measurements 
+# having no gaps is necessary for pooled cross validation scheme, so each 
+# subject has same number of rows of observations
 ################################################################################
 
-# how many subjects with 8 hours of data have no more than 3 min gap between 
+# how many subjects with 5 hours of data have no more than 1 min gap between 
 # consecutive observations? 
-eval_missingness(min = 3, dataset = mimic_all, total_hrs = 8)[["num"]] 
-# 786
-dat <- eval_missingness(min = 3, dataset = mimic_all, total_hrs = 8)[["dat"]] 
-
-# out of these 786 subjects how many times do they having 2 minute or greater 
-# gaps of missingness?  
-eval <- dat
-eval$tdiff <- unlist(tapply(eval$time_and_date, 
-                            INDEX = eval$subject_id,
-                            FUN = function(x) c(0, diff(as.numeric(x)))))
-missing <- eval %>% dplyr::filter(tdiff > 60)
-gaps_by_id <- missing %>%
-  dplyr::select("subject_id") %>%
-  dplyr::group_by(subject_id) %>%
-  tally()
-gaps_by_id$subject_id <- as.numeric(as.character(gaps_by_id$subject_id))
-subset(gaps_by_id, rowSums(gaps_by_id) - gaps_by_id$subject_id > 2)
-# not often, 3 times is the highest
+eval_missingness(min = 1, dataset = mimic_all, total_hrs = 8)[["num"]] 
+# 682
+dat <- eval_missingness(min = 1, dataset = mimic_all, total_hrs = 8)[["dat"]]
 
 ################################################################################
 # prep for combined SL
 ################################################################################
 
-# need "time" column 
-colnames(dat)[3] <- "time"
+# adding time column
+dat <- dat %>%
+  dplyr::group_by(subject_id) %>%
+  dplyr::mutate(time = seq(1,n(), 1))
 
-id <- "subject_id"
+# outcome needs to be numeric for correlation screener to work
+dat$hypo_event <- as.numeric(dat$hypo_event) - 1
 
 outcome <- "hypo_event"
 
@@ -74,18 +64,19 @@ metalearner <- make_learner(Lrnr_nnls)
 sl <- Lrnr_sl$new(learners = stack, metalearner = metalearner)
 
 ################################################################################
-# run combined SL with varying cv schemes and gaps
+# run combined SL with varying cv schemes and training times
 ################################################################################
 
 # we need at least gap + h time in test set, so we do not train on full 8 hrs
-# we need at least gap + test_size time in training set, so we do not include 30 min as a training time
+# we need at least gap + test_size time in training set, so we do not include 
+# 30 min as a training time
 times <- seq(30, 420, by = 30)[-1]
 
 comSL_cvrw <- lapply(times, function(x){
   combine_SL(train_all = dat, outcome, t = x, stack_pool = stack, 
              stack_individual = stack, stack_screen = stack_screen, sl = sl,
              covars = covars_timevarying, covars_baseline = covars_baseline,
-             id = id, cv = "folds_rolling_window", gap = 30, h = 15, 
+             cv = "folds_rolling_window", gap = 30, h = 15, 
              test_size = 15, mini_batch = 5, window_size = 15)
   })
 names(comSL_cvrw) <- paste0(times, " min training time")
@@ -96,7 +87,7 @@ comSL_cvro <- lapply(times, function(x){
   combine_SL(train_all = dat, outcome, t = x, stack_pool = stack, 
              stack_individual = stack, stack_screen = stack_screen, sl = sl,
              covars = covars_timevarying, covars_baseline = covars_baseline,
-             id = id, cv = "folds_rolling_origin", gap = 30, h = 15, 
+             cv = "folds_rolling_origin", gap = 30, h = 15, 
              test_size = 15, mini_batch = 5, window_size = 15)
 })
 names(comSL_cvro) <- paste0(times, " min training time")
