a31fd5f1abbdb067c7b9378f7ef10eb37710a900
Ivana <imalenica.stat@gmail.com>
2019-11-25 22:49:01 -0800

    Add custom functions for binary outcome

--- R/summarize_outcome.R
+++ R/summarize_outcome.R
@@ -3,6 +3,28 @@ summary_function <- function(vector, function_name) {
   FUN(vector) 
 }
 
+## Solution 1 - hypotensive episode for time t is defined as either:
+# 1. abpmean at time t < 65 mmHg and there is 5-minute window around time t 
+#    (i.e., 10 time-points) with at least 5 time-points abpmean < 65.
+#    Once the episodes are identified for the entire period, we summarize. 
+
+Y_sol1 = function(vec, window=5, cutoff=62){
+  
+  init <- ifelse(vec < cutoff,1,0)
+  
+  #Sum the lags around each possible event.
+  lags<-rowSums(as.data.frame(lapply(seq(1:window), function(x) lag(init,x))), na.rm = TRUE)
+  leads<-rowSums(as.data.frame(lapply(seq(1:window), function(x) lead(init,x))), na.rm = TRUE)
+  mids<-lags+leads
+  
+  event<-ifelse((init==1 & mids >= 5),1,
+                ifelse((init==0 & mids >= 8),1,0))
+  
+  #But now summarize over the entire period:
+  event<-ifelse(sum(event)>0,1,0)
+  return(event)
+}
+
 create_summary <- function(init, summary_window, summary_measure){
   cap <- nrow(init)-summary_window
 
@@ -14,22 +36,23 @@ create_summary <- function(init, summary_window, summary_measure){
   return(Y_summary)
 }
 
+#summary_measure: one of the standard stats functions, or custom function (Y_sol1)
 add_summary_outcome <- function(data, gap = 30, summary_window = 15, 
                                 summary_measure = "median"){
   dat <- data %>%
   group_by(subject_id) %>%
   mutate(Y = dplyr::lead(abpmean, n = gap, default = NA)) %>%
   mutate(Y_summary = NA)
-
-  lapply(unique(dat$subject_id), function(i){
+  
+  for(i in unique(dat$subject_id)){
     init <- na.omit(dat[dat$subject_id == i, "Y"])
     Y_summary <- create_summary(init=init, summary_window=summary_window, 
                                 summary_measure=summary_measure)
     #Pad the rest with NAs
     cap <- (nrow(dat[dat$subject_id == i,])-length(Y_summary))
     Y_summary <- c(Y_summary, rep(NA, cap))
-    dat[dat$subject_id == i,"Y_summary"] <- out
-  })
+    dat[dat$subject_id == i,"Y_summary"] <- Y_summary
+  }
 
   return(dat)
 }
