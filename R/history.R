f0c03a0d41c4cdc84d51cf0e6755e92129f80725
rachaelvphillips <rachaelvphillips@gmail.com>
2020-03-05 19:12:10 +0000

    future outcome

--- R/history.R
+++ R/history.R
@@ -1,31 +1,58 @@
 library(tidyverse)
 library(data.table)
 library(here)
+library(doParallel)
 
 load(here::here("Data","mimic.Rdata"))
 
-# fill in gaps in min_elapsed by creating new row and carring last row forward 
-dat <- data.table(mimic)
-setkey(dat, subject_id, min_elapsed)
-dat <- dat[CJ(unique(subject_id), seq(min(min_elapsed), max(min_elapsed))),
-           roll = TRUE] 
-
-# create `all_locf` column to indicate whether or not last row was carried forward
-anti_dat <- anti_join(dat, mimic)
-anti_dat$all_locf <- rep(1, nrow(anti_dat))
-semi_dat <- semi_join(dat, mimic)
-semi_dat$all_locf <- rep(0, nrow(semi_dat))
-dat <- full_join(anti_dat, semi_dat)
-
-dat <- dat[order(dat$subject_id, dat$time_and_date), ]
-
 ############################# add leaded outcome ###############################
-data <- dat %>%
-  group_by(subject_id) %>%
-  mutate(Y_15 = dplyr::lead(abpmean, n = 15, default = NA)) %>%
-  mutate(Y_20 = dplyr::lead(abpmean, n = 20, default = NA)) %>%
-  mutate(Y_25 = dplyr::lead(abpmean, n = 25, default = NA)) %>%
-  mutate(Y_30 = dplyr::lead(abpmean, n = 30, default = NA)) 
+
+dat <- data.table(mimic)
+dat_list <- split(dat, dat$subject_id)
+registerDoParallel(cores = (detectCores()-1))
+getDoParWorkers()
+added_outcome_list_all <- foreach(n = 1:length(dat_list)) %dopar% {
+  x <- dat_list[[n]]
+  added_outcome_list <- list()
+  for(i in 1:nrow(x)){
+    current <- x[i,]
+    gaps <- c(15,20,25,30)
+    future_outcomes_list <- list()
+    for(j in 1:length(gaps)){
+      # if the row for X-min ahead exists, add X-min ahead abpmean to current row
+      future <- current$min_elapsed + gaps[j]
+      if(length(which(x$min_elapsed == future)) > 0){
+        future_outcomes_list[[j]] <- x[which(x$min_elapsed == future),]$abpmean
+      } else {
+        future_outcomes_list[[j]] <- NA
+      }
+    }
+    future_outcomes <- unlist(future_outcomes_list)
+    names(future_outcomes) <- c(paste0("Y_", gaps))
+    added_outcome_list[[i]] <- data.frame(current, t(future_outcomes))
+  }
+  names(added_outcome_list) <- 1:nrow(x)
+  added_outcome <- bind_rows(added_outcome_list)
+  
+  comparison <- compare::compare(added_outcome[,-c(25:28)], x, allowAll = TRUE)
+  if(!(comparison$result)){
+    print(paste0("Non-equal comparison for subject_id", x$subject_id))
+  }
+  
+  if(length(unique(added_outcome$time_and_date)) != length(unique(x$time_and_date))){
+    print(paste0("Length of time_and_date not equal for subject_id", x$subject_id))
+  }
+  
+  if(length(unique(added_outcome$min_elapsed)) != length(unique(x$min_elapsed))){
+    print(paste0("Length of min_elapsed not equal for subject_id", x$subject_id))
+  }
+  return(added_outcome[order(added_outcome$time_and_date), ])
+}
+
+table(unlist(lapply(added_outcome_list_all, ncol)))
+
+mimic_outcome <- do.call(rbind, added_outcome_list_all)
+save(mimic_outcome, file = here("Data", "mimic_outcome.Rdata"), compress = TRUE)
 
 ########### add time series summaries / history to current time data ###########
 
@@ -40,20 +67,20 @@ data$ventilation <- as.numeric(data$ventilation)-1
 dat_trt <- data %>%
   group_by(subject_id) %>%
   mutate(a_last = dplyr::lag(amine, n = 1, default = NA)) %>% 
-  mutate(amine_switch1 = ifelse(amine-a_last == 1, min_elapsed-1, NA)) %>%
-  mutate(amine_switch0 = ifelse(amine-a_last == -1, min_elapsed-1, NA)) %>%
+  mutate(amine_switch1 = ifelse(amine-a_last == 1, min_elapsed, NA)) %>%
+  mutate(amine_switch0 = ifelse(amine-a_last == -1, min_elapsed, NA)) %>%
   mutate(s_last = dplyr::lag(sedation, n = 1, default = NA)) %>% 
-  mutate(sedation_switch1 = ifelse(sedation-s_last == 1, min_elapsed-1, NA)) %>%
-  mutate(sedation_switch0 = ifelse(sedation-s_last == -1, min_elapsed-1, NA)) %>%
+  mutate(sedation_switch1 = ifelse(sedation-s_last == 1, min_elapsed, NA)) %>%
+  mutate(sedation_switch0 = ifelse(sedation-s_last == -1, min_elapsed, NA)) %>%
   mutate(v_last = dplyr::lag(ventilation, n = 1, default = NA)) %>% 
-  mutate(ventilation_switch1 = ifelse(ventilation-v_last == 1, min_elapsed-1, NA)) %>%
-  mutate(ventilation_switch0 = ifelse(ventilation-v_last == -1, min_elapsed-1, NA)) %>%
+  mutate(ventilation_switch1 = ifelse(ventilation-v_last == 1, min_elapsed, NA)) %>%
+  mutate(ventilation_switch0 = ifelse(ventilation-v_last == -1, min_elapsed, NA)) %>%
   select(subject_id, min_elapsed, time_and_date,
          amine, amine_switch1, amine_switch0, 
          sedation, sedation_switch1, sedation_switch0,
          ventilation, ventilation_switch1, ventilation_switch0)
 
-dat_full <- left_join(dat_trt, data)
+dat_full <- left_join(data, dat_trt)
 dat <- dat_full[order(dat_full$subject_id, dat_full$time_and_date), ]
 
 ##### time-varying binary trt summaries
@@ -95,6 +122,36 @@ summarize_history_binary <- function(history_indicator_vector,
                   switch0_count = switch0_count)
   names(return_vec) <- paste0(name, names(return_vec))
   return(return_vec)
+}
+
+add_history_binary <- function(ind_data, min_history){
+  dat_ordered <- ind_data[order(ind_data$time_and_date), ]
+  
+  current_history_binary <- list()
+  for(i in 2:nrow(dat_ordered)){
+    history_full <- dat_ordered[c(1:(i-1)),]
+    # subset to history if i > min_history
+    if(i <= min_history){
+      history <- history_full
+    } else {
+      history <- history_full[c(
+        (nrow(history_full)-min_history+1):(nrow(history_full))),]
+    }
+    current <- dat_ordered[i,]
+    amine <- summarize_history_binary(history$amine, history$amine_switch1, 
+                                      history$amine_switch0, current$min_elapsed,
+                                      "amine_history_")
+    sed <- summarize_history_binary(history$sedation, history$sedation_switch1, 
+                                    history$sedation_switch0, current$min_elapsed,
+                                    "sedation_history_")
+    vent <- summarize_history_binary(history$ventilation, history$ventilation_switch1, 
+                                     history$ventilation_switch0, current$min_elapsed,
+                                     "ventilation_history_")
+    current_history_binary[[i]] <- c(current, amine, sed, vent)
+  }
+  df <- do.call(rbind, current_history_binary)
+  left_join(return_obj, dat_ordered[1,])
+  return(return_obj)
 }
 
 add_history_binary <- function(ind_data, min_history){
