6665b7373cc1c26506314293ade9d2032ea1463c
rachaelvphillips <rachaelvphillips@gmail.com>
2020-05-06 14:51:31 -0700

    fix process task

--- R/v3/process_task.R
+++ R/v3/process_task.R
@@ -1,31 +1,25 @@
 # solves issue with mismatch between historical and individual delta column
-process_task <- function(individual_training_task, historical_fit){
+process_task <- function(individual_training_task, historical_task){
   
-  hist_cols <- names(historical_fit$task$data)
+  hist_cols <- names(historical_task$data)
   ind_cols <- names(individual_training_task$data)
   ind_cols_miss <- hist_cols[!(hist_cols %in% ind_cols)]
-  delta_cols <- grep("delta_", ind_cols_miss)
-  
+
   if(length(ind_cols_miss) > 0){
   individual_training_data <- individual_training_task$data
   ind_cols_miss_extra <- data.frame(matrix(nrow=nrow(individual_training_data), 
                                            ncol=length(ind_cols_miss)))
   names(ind_cols_miss_extra) <- ind_cols_miss[seq(ind_cols_miss)]
   ind_cols_miss_extra[,ind_cols_miss] <- rep(0, nrow(individual_training_data))
-  
-  #training_task$add_columns(ind_cols_miss_extra)
   ind_train_data_new <- cbind.data.frame(individual_training_data, 
                                          ind_cols_miss_extra)
-  ind_train_data_new <- as.data.frame(ind_train_data_new)
-  
-  #Arrange to match historical:
-  output <- ind_train_data_new[,hist_cols]
   
   processed_task <- make_sl3_Task(
-    data =  output, 
-    covariates = historical_fit$task$nodes$covariates,
-    outcome = outcome, 
-    folds = individual_training_task$folds
+    data = data.table(ind_train_data_new), 
+    covariates = historical_task$nodes$covariates,
+    outcome = individual_training_task$nodes$outcome,
+    folds = individual_training_task$folds,
+    id = "subject_id"
   )
   } else {
     processed_task <- individual_training_task
