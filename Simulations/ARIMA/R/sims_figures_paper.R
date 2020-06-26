35ca2a56b6831654e57e912d26ec752cfd6fd7d5
Ivana <imalenica.stat@gmail.com>
2020-06-26 15:26:26 -0700

    Forecast vs truth plots

--- Simulations/ARIMA/R/sims_figures_paper.R
+++ Simulations/ARIMA/R/sims_figures_paper.R
@@ -232,3 +232,191 @@ p3 <- ggplot() +
         panel.grid = element_line(colour = "grey92"))
 
 ggarrange(p0, p1, p2, p3, nrow=2, ncol=2, common.legend = TRUE, legend="right", font.label="bold")
+
+#################################################################
+###   Plot forecast predictions vs. truth for one iteration
+#################################################################
+
+load(here("Simulations/ARIMA/Results/fit_preds_extra_v4.Rdata"))
+
+forecast_sim0 <- do.call(rbind,res_sim0$forecast)
+forecast_sim1 <- do.call(rbind,res_sim1$forecast)
+forecast_sim2 <- do.call(rbind,res_sim2$forecast)
+forecast_sim3 <- do.call(rbind,res_sim3$forecast)
+
+sz <- 1.1
+p0 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(forecast_sim0)),
+                y = round(forecast_sim0$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(forecast_sim0)),
+                y = round(forecast_sim0$persSL,0), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("Truth", "POSL")) +
+  ggtitle("(a) ARIMA process") +
+  ylim(60,90)+
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))
+
+p1 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(forecast_sim1)),
+                y = round(forecast_sim1$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(forecast_sim1)),
+                y = round(forecast_sim1$persSL,0), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ggtitle("(b) ARIMA process with X-dependent offset") +
+  ylim(60,90)+
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))  
+
+p2 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(forecast_sim2)),
+                y = round(forecast_sim2$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(forecast_sim2)),
+                y = round(forecast_sim2$persSL,0), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ylim(60,90)+
+  ggtitle("(c) Interrupted ARIMA process") +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))
+
+p3 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(forecast_sim3)),
+                y = round(forecast_sim3$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(forecast_sim3)),
+                y = round(forecast_sim3$persSL,0), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ylim(60,90)+
+  ggtitle("(d) Mixture of Gaussian AR models") +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92")) 
+
+ggarrange(p0, p1, p2, p3, nrow=2, ncol=2, common.legend = TRUE, legend="right", font.label="bold")
+
+#################################################################
+###   Plot CV predictions vs. truth for one iteration
+#################################################################
+
+load(here("Simulations/ARIMA/Results/fit_preds_extra_v4.Rdata"))
+
+sz <- 1.1
+p0 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(preds_sim0)),
+                y = round(preds_sim0$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(preds_sim0)),
+                y = round(preds_sim0$nnls_convexSL,2), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("Truth", "POSL")) +
+  ggtitle("(a) ARIMA process") +
+  ylim(60,90)+
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))
+
+p1 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(preds_sim1)),
+                y = round(preds_sim1$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(preds_sim1)),
+                y = round(preds_sim1$nnls_convexSL,2), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ggtitle("(b) ARIMA process with X-dependent offset") +
+  ylim(60,90)+
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))  
+
+p2 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(preds_sim2)),
+                y = round(preds_sim2$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(preds_sim2)),
+                y = round(preds_sim2$nnls_convexSL,2), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ylim(60,90)+
+  ggtitle("(c) Interrupted ARIMA process") +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92"))
+
+p3 <- ggplot() + 
+  geom_line(aes(x = as.numeric(row.names(preds_sim3)),
+                y = round(preds_sim3$truth,0), col = "Truth"), size=sz) +
+  geom_line(aes(x = as.numeric(row.names(preds_sim3)),
+                y = round(preds_sim3$nnls_convexSL,2), col = "POSL"), size=sz) +
+  xlab("") + ylab("Outcome") + 
+  scale_color_brewer(palette = "Dark2", name="Outcome", 
+                     labels = c("POSL", "Truth")) +
+  ylim(60,90)+
+  ggtitle("(d) Mixture of Gaussian AR models") +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(face="bold", angle=90, hjust=1),
+        axis.title.x = element_text(color="black", size=14, face="bold"),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        plot.title = element_text(color="black", size=16, face="bold"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1.3),
+        legend.title = element_text(colour="black", size=16, face="bold"), 
+        panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid = element_line(colour = "grey92")) 
+
+ggarrange(p0, p1, p2, p3, nrow=2, ncol=2, common.legend = TRUE, legend="right", font.label="bold")
+
+
+
+
+
