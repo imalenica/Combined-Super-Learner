c5479218c2a618bf061f4f1756ac33bde86c5410
rachaelvphillips <rachaelvphillips@gmail.com>
2020-10-07 14:59:21 -0500

    generalize args list

--- MIMICanalysis/3_run.R
+++ MIMICanalysis/3_run.R
@@ -1,16 +1,14 @@
 ################################### read args ##################################
-args <- R.utils::commandArgs(trailingOnly = TRUE, 
-                             asValues = TRUE,
-                             defaults = list(
-                               outcome = "Y15",
-                               historical_data = "historical30.Rdata",
-                               individual_data = "individual30.Rdata",
-                               individual_id = "791",
-                               outcome_locf = "abpmean_locf",
-                               outcome_gap = 15
-                               ))
-args <- list(outcome = "Y15", historical_data = "historical30.Rdata",
-             individual_data = "individual30.Rdata", individual_id = "791")
+# args <- R.utils::commandArgs(trailingOnly = TRUE, 
+#                              asValues = TRUE,
+#                              defaults = list(
+#                                outcome_gap = 15,
+#                                smooth = "none"
+#                                history = 30
+#                                individual_id = "791"))
+
+args <- list(outcome_gap = 15, smooth = "none", history = 30,
+             individual_id = "791")
 
 ############################ load libraries, data, source ######################
 library(data.table)
@@ -20,18 +18,38 @@ library(earth)
 
 # source(here::here("R", "setup.R"))
 # source(here::here("R", "sl_funs.R"))
-source(here::here("R", "v3", "2_sl3_setup.R"))
+source(here::here("MIMICanalysis", "2_sl3_setup.R"))
 source(here::here("R", "v3", "make_adapt_and_online_sl.R"))
 source(here::here("R", "v3", "utils_sl.R"))
 source(here::here("R", "v3", "process_task.R"))
 source(here::here("R", "v3", "get_weights.R"))
 # data_path <- "/global/scratch/rachelvphillips/symphony-data/"
 data_path <- "~/Downloads/"
-# data_path <- "~/symphony/data/"
 options(sl3.verbose = FALSE)
 
+if(args$history == 30){
+  individual_data_pre <- "individual30"
+  historical_data_pre <- "historical30"
+} else if(args$history == 60){
+  individual_data_pre <- "individual60"
+  historical_data_pre <- "historical60"
+}
+
+if(args$smooth == "mean"){
+  outcome <- paste0("Y", ((args$outcome_gap)+5), "_lag5_mean")
+  individual_data <- paste0(individual_data_pre, "_smooth_mean.Rdata")
+  historical_data <- paste0(historical_data_pre, "_smooth_mean.Rdata")
+} else if(args$smooth == "median"){
+  outcome <- paste0("Y", ((args$outcome_gap)+5), "_lag5_median")
+  individual_data <- paste0(individual_data_pre, "_smooth_median.Rdata")
+  historical_data <- paste0(historical_data_pre, "_smooth_median.Rdata")
+} else if(args$smooth == "none"){
+  outcome <- paste0("Y", args$outcome_gap)
+  individual_data <- paste0(individual_data_pre, ".Rdata")
+  historical_data <- paste0(historical_data_pre, ".Rdata")
+}
 ############################ load & subset individual data #####################
-individual_data_path <- paste0(data_path, args$individual_data)
+individual_data_path <- paste0(data_path, individual_data)
 load(individual_data_path)
 
 # subset to subject of interest
@@ -41,7 +59,7 @@ rm(individual)
 setorder(d, time_and_date)
 
 ############################## load historical fit #############################
-historical_fit_path <- paste0(data_path, args$outcome, "_", args$historical_data)
+historical_fit_path <- paste0(data_path, outcome, "_", historical_data)
 load(historical_fit_path)
 historical_fit <- fit # rename
 rm(fit)
@@ -69,12 +87,13 @@ if(max_time < 100) {
 splits <- seq((initial_burn_in+batch), max_time, batch)
 training_data_list <- lapply(splits, function(x) data.table(d[1:x, ]))
 forecast_data_list <- lapply(splits, function(x) data.table(d[(x+1):(x+batch), ]))
+rm(d)
 
 # function to determine new cv stacks based on sample size
 get_cv_stack_group <- function(n) ifelse(n<50, 1, ifelse(n>=50 && n<200, 2, 3))
 
 # function to increase burn_in (i.e., initial training size) w complex cv stacks
-get_burn_in <- function(n) ifelse(n<200, initial_burn_in, initial_burn_in*5)
+get_burn_in <- function(n) ifelse(n<200, initial_burn_in, (initial_burn_in*5))
 
 ################################## run #########################################
 
@@ -84,9 +103,11 @@ onlineSL_forecasts <- list()
 individual_fits <- list()
 all_forecasts <- list()
 
+set.seed(518)
+
 sl3_debug_mode()
 # N <- length(splits)-1
-N <- 2
+N <- 5
 start_time <- proc.time()
 for(i in 1:N){
   
@@ -106,7 +127,7 @@ for(i in 1:N){
   if(i == 1){
     cv_stack <- make_individual_cv_stack(n = splits[[i]])
     fit <- invisible(make_adapt_and_online_sl(
-      individual_training_data = training_data, outcome = args$outcome, 
+      individual_training_data = training_data, outcome = outcome, 
       id = args$individual_id, covariates = covs, 
       historical_fit = historical_fit, batch = batch, first_window = burn_in, 
       individual_stack = cv_stack, individual_forecast_data = forecast_data, 
@@ -122,7 +143,7 @@ for(i in 1:N){
       cv_stack <- make_individual_cv_stack(n = splits[[i]])
     }
     fit <- invisible(make_adapt_and_online_sl(
-      individual_training_data = training_data, outcome = args$outcome, 
+      individual_training_data = training_data, outcome = outcome, 
       id = args$individual_id, covariates = covs, batch = batch, 
       first_window = burn_in, historical_fit = historical_fit, 
       individual_forecast_data = forecast_data, individual_stack = cv_stack, 
@@ -138,32 +159,15 @@ for(i in 1:N){
   all_forecasts[[i]] <- fit$all_forecasts
 }
 end_time <- proc.time() - start_time
+end_time
 
-# quick summary of the individual
-d <- training_data_list[[N]]
-summary_table <- data.table(
-  subject_id = unique(d$subject_id), icustay_id = unique(d$icustay_id), 
-  id = unique(d$id), abpmean_mean = mean(d$abpmean), amine_mean = mean(d$amine), 
-  sex = unique(d$sex), age = unique(d$age), bmi = unique(d$bmi), 
-  sapsi_first = unique(d$sapsi_first), sofa_first = unique(d$sofa_first), 
-  care_unit = unique(d$care_unit), 
-  admission_type_descr = unique(d$admission_type_desc)
-)
-
-locf <- ifelse(d[[args$outcome_locf]] == 1|| d[["row_locf"]] == 1, 1, 0)
-locf <- locf[args$outcome_gap:nrow(d)]
-locf <- c(locf, rep(NA, nrow(d)-length(locf)))
-Y_tbl <- data.table(truth = d[[args$outcome]], locf = locf, time = d[["min"]])
-pred_tbl <- rbindlist(all_forecasts, fill=T)
-forecast_tbl <- data.table(merge(Y_tbl, pred_tbl, all.x=F, all.y=T, by="time"))
-
+forecast_tbl <- rbindlist(all_forecasts, fill=T)
 results <- list(
-  id_summary = summary_table,
   onlineSL_table = onlineSL_tables[[N]],
   loss_table = loss_tables[[N]],
   forecast_table = forecast_tbl
 )
 
 ################################## save ########################################
-path <- paste0(args$outcome, "_id", args$individual_id, ".Rdata")
+path <- paste0(outcome, "_id", args$individual_id, ".Rdata")
 save(results, file=paste0(data_path, path), compress=T)
