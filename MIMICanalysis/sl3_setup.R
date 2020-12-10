58084d706b9dc4628abf4554f7722e25a9fa975d
rachaelvphillips <rachaelvphillips@gmail.com>
2020-12-10 15:44:22 -0800

    simplify individual lib

--- MIMICanalysis/sl3_setup.R
+++ MIMICanalysis/sl3_setup.R
@@ -1,3 +1,11 @@
+get_covariates <- function(data){
+  outcomes <- colnames(data)[grepl("Y", colnames(data))]
+  not_covs <- c(outcomes, "time_and_date", "init_time_and_date", "subject_id", 
+                "icustay_id", "id")
+  covs <- colnames(data)[-which(colnames(data) %in% not_covs)]
+  return(covs)
+}
+
 make_historical_stack <- function(){
   
   # learners with no internal screening
@@ -16,10 +24,7 @@ make_historical_stack <- function(){
   
   grid_params <- list(alpha = seq(0, 1, 0.1))
   grid <- expand.grid(grid_params, KEEP.OUT.ATTRS=F)
-  params_default <- list(nthread = getOption("sl.cores.learners", 1))
-  glmnets <- apply(grid, 1, function(params_tune) {
-    do.call(Lrnr_glmnet$new, c(params_default, as.list(params_tune)))
-  })
+  glmnets <- apply(grid, 1, function(par) do.call(Lrnr_glmnet$new, as.list(par)))
   
   db2 <- Lrnr_dbarts$new(k=2, ntree=200, ndpost=1000, nskip=100, keeptrees=T)
   db.5 <- Lrnr_dbarts$new(k=0.5, ntree=200, ndpost=1000, nskip=100, keeptrees=T)
@@ -47,52 +52,47 @@ make_historical_stack <- function(){
   return(make_learner(Pipeline, screener_augmented_rf, stack))
 }
 
-get_covariates <- function(data){
-  outcomes <- colnames(data)[grepl("Y", colnames(data))]
-  not_covs <- c(outcomes, "time_and_date", "init_time_and_date", "subject_id", 
-                "icustay_id", "id")
-  covs <- colnames(data)[-which(colnames(data) %in% not_covs)]
-  return(covs)
-}
-
 make_individual_cv_stack <- function() {
   
-  # no internal screening stack
+  ##### learners that accommodate external regressors
+  # no internal covariate screening
   lrn_glm <- Lrnr_glm$new()
-  lrn_mean <- Lrnr_mean$new()
   arima_aicc <- Lrnr_arima$new(stepwise=F, approximation=F, ic="aicc")
   arima_bic <- Lrnr_arima$new(stepwise=F, approximation=F, ic="bic")
-  lrnrs_no_screen <- c(lrn_glm, lrn_mean, arima_aicc, arima_bic)
-  names(lrnrs_no_screen) <- c("glm", "mean", "arima_aicc", "arima_bic")
+  lrnrs_no_screen <- c(lrn_glm, arima_aicc, arima_bic)
+  names(lrnrs_no_screen) <- c("glm", "arima_aicc", "arima_bic")
   stack_no_screen <- make_learner(Stack, lrnrs_no_screen)
   
   lasso <- Lrnr_glmnet$new()
   screener_lasso <- Lrnr_screener_coefs$new(lasso, threshold=0)
   pipe_screen <- make_learner(Pipeline, screener_lasso, stack_no_screen)
   
-  # internal screening stack
-  grid_params <- list(alpha=seq(0, 1, 0.2))
+  # internal screening 
+  grid_params <- list(alpha = seq(0, 1, 0.2))
   grid <- expand.grid(grid_params, KEEP.OUT.ATTRS=F)
-  params_default <- list(nthread = getOption("sl.cores.learners", 1))
-  glmnets <- apply(grid, 1, function(params_tune) {
-    do.call(Lrnr_glmnet$new, c(params_default, as.list(params_tune)))
-  })
+  glmnets <- apply(grid, 1, function(par) do.call(Lrnr_glmnet$new, as.list(par)))
   
   rf <- Lrnr_ranger$new()
-  xgb <- Lrnr_xgboost$new(max_depth=5, eta=0.1, nrounds=100, 
+  
+  xgb <- Lrnr_xgboost$new(max_depth=4, eta=0.1, nrounds=100,
                           colsample_bytree=0.8, early_stopping_rounds=25)
-  nlts_setar <- Lrnr_tsDyn$new(learner="setar", model="MTAR")
-  nlts_nnet15 <- Lrnr_tsDyn$new(learner="nnetTs", m=15, size=5)
-  nlts_nnet25 <- Lrnr_tsDyn$new(learner="nnetTs", m=25, size=5)
-  lrnrs <- c(xgb, glmnets, rf, nlts_lstar15, nlts_lstar25, nlts_setar, 
-             nlts_nnet15, nlts_nnet25, pipe_screen)
+  
+  ##### learners that DO NOT accommodate external regressors   
+  nlts_setar <- Lrnr_tsDyn$new(learner="setar", m=1)
+  nlts_nnet5 <- Lrnr_tsDyn$new(learner="nnetTs", m=5, size=5, control=list(trace=F))
+  nlts_nnet15 <- Lrnr_tsDyn$new(learner="nnetTs", m=15, size=5, control=list(trace=F))
+  lrn_mean <- Lrnr_mean$new()
+  
+  ##### put it all together        
+  lrnrs <- c(glmnets, xgb, rf, nlts_setar, nlts_nnet5, nlts_nnet15, lrn_mean, 
+             pipe_screen)
   names(lrnrs) <- c(
-    "xgb", "glmnet_0", "glmnet_0.2",  "glmnet_0.4", "glmnet_0.6", "glmnet_0.8", 
-    "glmnet_1", "ranger", "nlts_lstar15", "nlts_lstar25", "nlts_setar", 
-    "nlts_nnet15", "nlts_nnet25", "lasso_screen"
+    "glmnet_0", "glmnet_0.2", "glmnet_0.4", "glmnet_0.6",  "glmnet_0.8", 
+    "glmnet_1", "xgboost", "ranger", "nlts_setar", "nlts_nnet5", "nlts_nnet15", 
+    "mean", "lasso_screen"
   )
   stack <- make_learner(Stack, lrnrs)
-  cv_stack <- Lrnr_cv$new(stack, full_fit = T)
+  cv_stack <- Lrnr_cv$new(stack)
   # all_lrnrs <- c(cv_stack, historical_fit)
   # names(all_lrnrs) <- c("individual", "historical")
   # final_stack <- make_learner(Stack, all_lrnrs)
